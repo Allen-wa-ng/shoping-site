038824974e31da6f5eba74f8d439a904cb98bea0
Allen-wa-ng <wang0911309998@gmail.com>
2022-12-27 22:25:01 +0800

    little adjustment

--- 桂來標網站/single.php
+++ 桂來標網站/single.php
@@ -153,6 +153,10 @@
 					<div class="row animate-box">
 						<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
 							<h2>Hauteville Rocking Chair</h2>
+							<?php
+							
+							?>
+
 							<h2>$ 350 / 每斤</h2>
 							<p>
 								<a href="#" class="btn btn-primary btn-outline btn-lg">Add to Cart</a>
@@ -255,89 +259,32 @@
 		</div>
 	</div>
 
-	<div id="fh5co-started">
-		<div class="container">
-			<div class="row animate-box">
-				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-					<h2>Newsletter</h2>
-					<p>Just stay tune for our latest Product. Now you can subscribe</p>
-				</div>
-			</div>
-			<div class="row animate-box">
-				<div class="col-md-8 col-md-offset-2">
-					<form class="form-inline">
-						<div class="col-md-6 col-sm-6">
-							<div class="form-group">
-								<label for="email" class="sr-only">Email</label>
-								<input type="email" class="form-control" id="email" placeholder="Email">
-							</div>
-						</div>
-						<div class="col-md-6 col-sm-6">
-							<button type="submit" class="btn btn-default btn-block">Subscribe</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-
 	<footer id="fh5co-footer" role="contentinfo">
-		<div class="container">
-			<div class="row row-pb-md">
-				<div class="col-md-4 fh5co-widget">
-					<h3>Shop.</h3>
-					<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
-				</div>
-				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-					<ul class="fh5co-footer-links">
-						<li><a href="#">About</a></li>
-						<li><a href="#">Help</a></li>
-						<li><a href="#">Contact</a></li>
-						<li><a href="#">Terms</a></li>
-						<li><a href="#">Meetups</a></li>
-					</ul>
-				</div>
-
-				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-					<ul class="fh5co-footer-links">
-						<li><a href="#">Shop</a></li>
-						<li><a href="#">Privacy</a></li>
-						<li><a href="#">Testimonials</a></li>
-						<li><a href="#">Handbook</a></li>
-						<li><a href="#">Held Desk</a></li>
-					</ul>
-				</div>
-
-				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-					<ul class="fh5co-footer-links">
-						<li><a href="#">Find Designers</a></li>
-						<li><a href="#">Find Developers</a></li>
-						<li><a href="#">Teams</a></li>
-						<li><a href="#">Advertise</a></li>
-						<li><a href="#">API</a></li>
-					</ul>
-				</div>
-			</div>
-
 			<div class="row copyright">
 				<div class="col-md-12 text-center">
 					<p>
-						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
-						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://blog.gessato.com/" target="_blank">Gessato</a> &amp; <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+						<small class="block">&copy; 桂來標湖南臘肉</small>
+						<small class="block">地址：
+							<a href="https://www.google.com/maps/dir/25.0871808,121.5496192/25.1156834,121.5157655/@25.0978977,121.5174386,14z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0"
+								target="_blank">
+								石牌路二段(裕民四路口)
+							</a>
 					</p>
 					<p>
-						<ul class="fh5co-social-icons">
-							<li><a href="#"><i class="icon-twitter"></i></a></li>
-							<li><a href="#"><i class="icon-facebook"></i></a></li>
-							<li><a href="#"><i class="icon-linkedin"></i></a></li>
-							<li><a href="#"><i class="icon-dribbble"></i></a></li>
-						</ul>
+					<ul class="fh5co-social-icons">
+						<!-- <li><a href="#"><i class="icon-twitter"></i></a></li> -->
+						<li><a href="https://www.facebook.com/profile.php?id=100063940123246"><i
+									class="icon-facebook"></i></a>
+						</li>
+						<li><a
+								href="https://www.google.com/maps/dir/25.0871808,121.5496192/25.1156834,121.5157655/@25.0978977,121.5174386,14z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0"><i
+									class="icon-globe2"></i></a></li>
+						<li><a href="tel://0926166318"><i class="icon-phone"></i></a></li>
+					</ul>
 					</p>
 				</div>
 			</div>
-
-		</div>
-	</footer>
+		</footer>
 	</div>
 
 	<div class="gototop js-top">
